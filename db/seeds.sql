7944f80b2d1caeaecda9391b4f14e4da5fa71b13
Long Nguyen <Long.Nguyen@temple.edu>
2020-03-15 16:52:12 -0400

    Initial commit plus completed app, db, config, model, controller, and view setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Double Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", true);
